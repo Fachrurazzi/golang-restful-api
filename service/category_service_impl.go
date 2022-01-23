cf339eb3b98f7b99bded3f0488f7c0c399b51781
Fachrurazzi <razzytirta@gmail.com>
2022-01-23 17:57:09 +0800

    Category Validation in CategoryServiceImpl Create & Update

--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -7,14 +7,18 @@ import (
 	"github.com/Fachrurazzi/golang-restful-api/model/domain"
 	"github.com/Fachrurazzi/golang-restful-api/model/web"
 	"github.com/Fachrurazzi/golang-restful-api/repository"
+	"github.com/go-playground/validator/v10"
 )
 
 type CategoryServiceImpl struct {
 	CategoryRepository repository.CategoryRepository
 	DB                 *sql.DB
+	Validate           *validator.Validate
 }
 
 func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
+	err := service.Validate.Struct(request)
+	helper.PanicIfError(err)
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
@@ -30,6 +34,7 @@ func (service *CategoryServiceImpl) Create(ctx context.Context, request web.Cate
 }
 
 func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
+	service.Validate.Struct(request)
 	tx, err := service.DB.Begin()
 	helper.PanicIfError(err)
 	defer helper.CommitOrRollback(tx)
